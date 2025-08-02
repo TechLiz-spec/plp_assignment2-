c124750b6466ca77456975fb33e10729c40d3c75
Elizabeth Ayucha <ayuchaelizabeth@gmail.com>
2025-08-02 14:43:28 +0300

    my_list.py

---
+++ my_list.py
@@ -0,0 +1,26 @@
+my_list=[]
+#print(my_list)
+#my_list.append(10 , 20 , 30 , 40 )
+#print(my_list)
+#my_list.append(10) 
+#print(my_list)
+#my_list.append(20)
+#print(my_list)
+#my_list.append(30)
+#my_list.append(40)
+#print(my_list)
+#my_list(1)=15
+#print(my_list)
+#my_list[1]=15
+#print(my_list)
+my_list=[10 , 20 , 30 ,40 ]
+print(my_list)
+my_list[1]=15
+print(my_list)
+my_list.extend([50 , 60 , 70])
+print(my_list)
+del my_list[6]
+print(my_list)
+my_list.sort()
+print(my_list)
+print(my_list.index(30))
